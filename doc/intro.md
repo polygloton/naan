f2a9a033ac02ff87678684329f97c57c0f04cbba
Stephen <steve@stevesloan.com>
2013-08-12 11:23:49 -0700

    lein new

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to naan
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/naan/core_test.clj
@@ -0,0 +1,7 @@
+(ns naan.core-test
+  (:require [clojure.test :refer :all]
+            [naan.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
